882027ca9237985bee6ee77e99d8a6b94b68ea1a
Ashis Chaudhary <ashischaudhary959@gmail.com>
2022-11-16 17:04:39 +1100

    added schema for tokens

--- models/tokenModel.js
+++ models/tokenModel.js
@@ -1,7 +1,18 @@
 const mongoose = require("mongoose");
 const crypto = require("crypto");
 
-const tokenSchema = new mongoose.model({
+//initialize both dates
+const date = new Date();
+const extendedDate = new Date();
+
+//get current day of the current month
+const currentDate = extendedDate.getDate();
+const extendDay = 21;
+
+//set extendedDate by adding extra days
+extendedDate.setDate(currentDate + extendDay);
+
+const tokenSchema = new mongoose.Schema({
   access_token: {
     type: String,
     required: true,
@@ -10,16 +21,17 @@ const tokenSchema = new mongoose.model({
   create_date: {
     type: Date,
     required: true,
-    default: Date.now,
+    default: date,
   },
   expire_date: {
-    type: String,
+    type: Date,
     required: true,
-    default: Date.now,
+    default: extendedDate,
   },
   is_valid: {
     type: Boolean,
     required: true,
+    default: true,
   },
   user_id: {
     type: String,
