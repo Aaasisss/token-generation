b09fea920fcd41ad0192fb18e0cb47929ffbca1c
Ashis Chaudhary <ashischaudhary959@gmail.com>
2022-11-20 17:58:50 +1100

    use bcrypt to hash the original access token

--- routes/tokens.js
+++ routes/tokens.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const Token = require("../models/tokenModel");
 const router = express.Router();
+const crypto = require("crypto");
+const bcrypt = require("bcrypt");
 
 //get all the tokens
 router.get("/", async (req, res) => {
@@ -8,7 +10,7 @@ router.get("/", async (req, res) => {
     const tokens = await Token.find();
     res.status(200).send({
       status: "200",
-      Data: tokens,
+      data: tokens,
       message: "success",
     });
   } catch (error) {
@@ -22,7 +24,7 @@ router.get("/:id", getToken, async (req, res) => {
   try {
     res.status(200).send({
       status: "200",
-      Data: token,
+      data: token,
       message: "success",
     });
   } catch (error) {
@@ -32,26 +34,55 @@ router.get("/:id", getToken, async (req, res) => {
 
 //add a token
 router.post("/", async (req, res) => {
-  const token = new Token({
-    user_id: req.body.user_id,
-  });
-  try {
-    const newToken = await token.save();
-    res.status(200).send({
-      status: "200",
-      Data: newToken,
-      message: "token saved successfully.",
+  const token_name = req.body.token_name;
+  const user_id = req.body.user_id;
+
+  let randomUUid = crypto.randomUUID();
+
+  const user_id_15_char = user_id.slice(0, 15);
+  const date = String(Date.now());
+
+  //create a token by combining user_id, data and randomUUID
+  const raw_token = `${user_id_15_char}.${date}.${randomUUid}`;
+
+  bcrypt.hash(raw_token, 10, async function (err, hashed_token) {
+    if (err) {
+      res.status(500).json({ message: err.message });
+    }
+
+    // create token object to store in DB
+    const token = new Token({
+      token_name: token_name,
+      user_id: user_id,
+      access_token: hashed_token,
     });
-  } catch (error) {
-    res.status(400).json({ message: error.message });
-  }
+
+    try {
+      //save the token to DB
+      const new_token = await token.save();
+
+      //replace hashed token value to raw_token to send back to user
+      new_token.access_token = raw_token;
+
+      res.status(200).send({
+        status: "200",
+        data: new_token,
+        message: "token saved successfully.",
+      });
+    } catch (error) {
+      res.status(400).json({ message: error.message });
+    }
+  });
 });
 
 //modify token details with id
 router.put("/:id", getToken, async (req, res) => {
   const token = res.token;
-  const { user_id, expire_date, is_valid } = req.body;
+  const { token_name, user_id, expire_date, is_valid } = req.body;
 
+  if (token_name) {
+    token.token_name = token_name;
+  }
   if (user_id) {
     token.user_id = user_id;
   }
@@ -66,7 +97,7 @@ router.put("/:id", getToken, async (req, res) => {
     await token.save();
     res.status(200).send({
       status: "200",
-      Data: token,
+      data: token,
       message: "token updated successfully",
     });
   } catch (error) {
@@ -81,7 +112,7 @@ router.delete("/:id", getToken, async (req, res) => {
     await token.delete();
     res.status(200).send({
       status: "200",
-      Data: token,
+      data: token,
       message: "token deleted successfully",
     });
   } catch (error) {
